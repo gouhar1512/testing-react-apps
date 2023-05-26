c7702bd89a566c272dcf26b0c34302270b60ed5d
gouhar1512 <ushran987@gmail.com>
2023-05-26 22:51:50 +0530

    Refactored a bit

--- src/__test__/UserList.test.js
+++ src/__test__/UserList.test.js
@@ -1,13 +1,19 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "../components/UserList";
 
-test("render one row per user", () => {
-  // Render the component
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "sam", email: "sam@sam.com" },
   ];
   render(<UserList users={users} />);
+
+  return { users };
+}
+
+test("render one row per user", () => {
+  // Render the component
+  renderComponent();
 
   // Find all the rows in the table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
@@ -17,11 +23,7 @@ test("render one row per user", () => {
 });
 
 test("render the email and name of each user", () => {
-  const users = [
-    { name: "jane", email: "jane@jane.com" },
-    { name: "sam", email: "sam@sam.com" },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
